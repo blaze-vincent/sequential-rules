946bdc8a922b55afd4f75766c96670cfacb0eb5a
blaze vincent <blaze.vincent@hotmail.com>
2025-03-26 14:22:43 -0500

    Appease linter

--- src/main/java/dev/n1t/validator/RuleTarget.java
+++ src/main/java/dev/n1t/validator/RuleTarget.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public abstract class RuleTarget {
-    private List<String> rulesFired = Collections.synchronizedList(new ArrayList<>());
+    private final List<String> rulesFired = Collections.synchronizedList(new ArrayList<>());
 
     public List<String> getRulesFired(){
         return rulesFired;
@@ -13,8 +13,7 @@ public abstract class RuleTarget {
 
     public void addRulesFired(String ...ruleNames){
         synchronized (rulesFired) {
-            for(String ruleName : ruleNames)
-                rulesFired.add(ruleName);
+            Collections.addAll(rulesFired, ruleNames);
         }
     }
 }
