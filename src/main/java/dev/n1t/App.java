13fa7aa7b4e80433c58e64715240193ad17e0c8d
blaze vincent <blaze.vincent@hotmail.com>
2025-04-26 16:58:32 -0500

    Delete example classes to declutter

--- src/main/java/dev/n1t/App.java
+++
@@ -1,9 +0,0 @@
-package dev.n1t;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
--- src/main/java/dev/n1t/Message.java
+++
@@ -1,3 +0,0 @@
-package dev.n1t;
-
-public record Message(String ruleName, String text, Object value) {}
